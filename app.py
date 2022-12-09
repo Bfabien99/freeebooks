86cb3b7d405ba7a638adb15904abc68d05092d9e
bfabien99 <fabienbrou99@gmail.com>
2022-12-09 15:46:53 +0000

    call api in my webapp

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, redirect, url_for
+import urllib.request, json
 
 # initialisation de flask
 app = Flask(__name__)
@@ -8,6 +9,15 @@ app = Flask(__name__)
 @app.route('/', methods=["GET"])
 def index():
     return render_template("index.html")
+
+@app.route('/books', methods=["GET"])
+def books():
+    url = "http://localhost:5000/books"
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    dict = json.loads(data)
+    books=dict["result"]
+    return render_template("book.html",books=books)
 
 
 if __name__=='__main__':
